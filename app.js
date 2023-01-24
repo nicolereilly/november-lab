38e518041cfd73ea6b3b352f503d6eef2d27ea51
Reilly <nreilly@una.edu>
2023-01-24 16:40:49 -0600

    this is working now

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;  
 
+
+app.set('view engine', 'ejs');
+let myName = 'Nicole';
+
 app.get('/', (req, res) => {
-  res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
+  //res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
+  res.send(`<h3> Hi, ${myName} </h3>`)
 })
 
 
@@ -14,6 +19,11 @@ app.get('/show', (req, res) => {
     res.sendFile('index.html' , { root : __dirname});
 
 
+})
+
+app.get('/ejs', (req, res) => {
+    //ejs stuff goes here
+    res.render('pages/about');
 })
 
 console.log('in the node console');
